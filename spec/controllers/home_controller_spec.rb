5eda6ea1b6f1573ea6e321d42caf2749318dbe37
Ernesto Soberanes <netosober@gmail.com>
2014-07-08 07:11:09 -0400

    Add home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/").to route_to(
+        :controller => "home",
+        :action => "index")
+    end
+  end
 
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
